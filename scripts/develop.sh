9362b918e1d7cf934da3f201cd3dec504ab2b306
Daniel P. Shannon <daniel@try.works>
2023-08-31 22:13:17 -0400

    🔨 Dockerize.

---
+++ scripts/develop.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+set -Eeuxo pipefail
+
+args="$*"
+
+restart_server() {
+    echo "Starting development server..."
+    pkill -f "yarn.js dev" || true
+
+    [[ -e "/run/secrets/environment" ]] || { echo "Missing environment secrets." && exit 1; }
+    source /run/secrets/environment && export GITHUB_TOKEN FONT_AWESOME_NPM_TOKEN GSAP_NPM_TOKEN
+    yarn dev &
+    disown
+}
+
+configure_watches() {
+    echo "Configuring watches..."
+
+    watchman watch-del-all || true
+    watchman watch-project "$PWD"
+    for j in scripts/watchman/*.json; do
+        echo "Setting watch $j"
+        watchman -j <"$j"
+    done
+}
+
+watch_watchman() {
+    pkill -f watchman || true
+    watchman --logfile=- --log-level=debug --foreground watch-project /app
+}
+
+yarn_install() {
+    echo "Running yarn install..."
+    [[ -e "/run/secrets/environment" ]] || { echo "Missing environment secrets." && exit 1; }
+    source /run/secrets/environment && export FONT_AWESOME_NPM_TOKEN GSAP_NPM_TOKEN GITHUB_TOKEN
+    yarn install
+}
+
+case $args in
+serve)
+    restart_server
+    ;;
+
+watch)
+    yarn_install
+    restart_server
+    configure_watches
+    watch_watchman
+    ;;
+
+watches)
+    configure_watches
+    ;;
+
+yarn)
+    yarn_install
+    restart_server
+    ;;
+
+*)
+    echo "Unknown command: $args"
+    ;;
+esac
--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,6 @@ export default defineConfig({
         ],
     },
     server: {
-        host: '::1',
+        host: true,
     },
 })
