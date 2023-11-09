3962f0a3ee42f736dc66d6544fc697b77dc5fcfd
Daniel P. Shannon <daniel@try.works>
2023-11-09 06:13:34 -0500

    💄 Position copy responsively with center shim.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,36 +3,84 @@
     <meta name='description' content='Software engineering portfolio for Daniel P. Shannon' />
 </svelte:head>
 
-<header>
-    <div class='dek'>
-        <h3>
-            I’m a full-stack engineer and architect who brings insight and rigor to
-            ambitious software projects.
-        </h3>
-    </div>
-    <div class='hed'>
-        <h1>
-            You can call me Daniel.
-        </h1>
-    </div>
-    <!-- <div class='shim' /> -->
-</header>
+<section class='intro'>
+    <header>
+        <div class='dek'>
+            <h3>
+                I’m a full-stack engineer and architect who brings insight and rigor to
+                ambitious software.
+            </h3>
+        </div>
+        <div class='hed'>
+            <h1>
+                You can call me Daniel.
+            </h1>
+        </div>
+        <div class='shim' />
+    </header>
+</section>
 
 <style lang='stylus'>
     @import 'theme/index.styl'
 
-    header
-        position fixed
-        top 50%
-        left 0
+    .intro
+        position absolute
 
-        transform translateY(-50%)
+        display flex
+        flex-direction row
+        align-items stretch
+        justify-content flex-start
+        width 100vw
+        height 100vh
+        padding 0 1rem
+
+    .intro header
+        display flex
+        flex-direction column
+        align-items stretch
+        justify-content flex-end
+        padding 0 5% 0 0
+
+        +above(md)
+            padding 0 10% 0 0
+
+        +above(lg)
+            padding 0 20% 0 0
 
     h1
-        plumber-charlie(font-size: 6, line-height: 6)
+        plumber-charlie(font-size: 3, line-height: 4)
         font-weight 300
 
+        +above(md)
+            plumber-charlie(font-size: 4, line-height: 5)
+
+        +above(lg)
+            plumber-charlie(font-size: 7, line-height: 8)
+
+    .hed
+        display flex
+        flex 1 0 auto
+        flex-direction column
+        align-items stretch
+        justify-content flex-start
+
     h3
-        plumber-maison(font-size: 3, line-height: 4)
-        font-weight 400
+        plumber-maison(font-size: 2, line-height: 3)
+        font-weight 200
+
+        +above(md)
+            plumber-maison(font-size: 3, line-height: 4)
+
+        +above(lg)
+            plumber-maison(font-size: 4, line-height: 5)
+
+    .dek
+        display flex
+        flex 0 1 50%
+        flex-direction column
+        align-items stretch
+        justify-content flex-end
+
+    .shim
+        flex 1 1 50%
 </style>
